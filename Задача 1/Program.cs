f9cf36b988a7bfd0e98505b26257a23660f32a78
BR <244097@niuitmo.ru>
2024-03-11 15:27:08 +0300

    Добавьте файлы проекта.

---
+++ Задача 1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.IO;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+
+namespace Задача_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int numberOfProfucts = 5;
+            Product[] products = new Product[numberOfProfucts];
+            for (int i = 0; i < numberOfProfucts; i++)
+            {
+                Console.WriteLine("Введите код");
+                int code = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите наименование");
+                string name = Console.ReadLine();
+                Console.WriteLine("Введите цену");
+                double cost = Convert.ToDouble(Console.ReadLine());
+                products[i] = new Product { Code = code, Name = name, Cost = cost };
+            }
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+                WriteIndented = true
+            };
+
+            string json = JsonSerializer.Serialize(products, options);
+            string path = "../../../Products.json";
+            using (StreamWriter sw = new StreamWriter(path, false))
+            {
+                sw.WriteLine(json);
+            }
+
+        }
+    }
+}
---
+++ Задача 2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Text.Json;
+
+namespace Задача_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "../../../Products.json";
+            string text = String.Empty;
+            using (StreamReader sr = new StreamReader(path))
+            {
+                text = sr.ReadToEnd();
+            }
+            Product[] readingProducts = new Product[text.Length];
+            readingProducts = JsonSerializer.Deserialize<Product[]>(text);
+            Product expensiveProduct = new Product();
+            expensiveProduct = readingProducts[0];
+            foreach (Product p in readingProducts)
+            {
+                if (p.Cost> expensiveProduct.Cost)
+                    expensiveProduct = p;
+            }
+            Console.WriteLine($" Название самого дорого товара - {expensiveProduct.Name}");
+            Console.ReadKey();
+        }
+    }
+}
